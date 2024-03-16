445278f3312ae5976cbdc4c84120d32a61dde77b
otakakot <kotaro.otaka@gmail.com>
2024-03-16 21:49:13 +0900

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,6 @@ ${SECRET_KEY}
 EOF
 `
 
-payload=`cat <<EOF | jq
-${CLIENT_PAYLOAD}
-EOF
-`
-
 go install github.com/otakakot/repository-dispatch@latest
 
-repository-dispatch --app-id ${APP_ID} --event-type ${EVENT_TYPE} --repository-owner ${REPOSITORY_OWNER} --repository-name ${REPOSITORY_NAME} --secret-key ${key} --client-payload ${payload}
+repository-dispatch --app-id ${APP_ID} --event-type ${EVENT_TYPE} --repository-owner ${REPOSITORY_OWNER} --repository-name ${REPOSITORY_NAME} --secret-key "${key}" --client-payload ${CLIENT_PAYLOAD}
